b92584d98efa69578927adfa9c28719777197298
trungnd11 <trungnd1@devcamp.edu.vn>
2023-01-08 22:09:50 +0700

    responsive login page

--- src/component/variable.ts
+++ src/component/variable.ts
@@ -21,3 +21,24 @@ export const HeaderComponent = {
 export const BorderRadius = {
   border6: "6px",
 };
+
+// Responsives
+const sizes = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "2560px",
+};
+
+export const Devices = {
+  mobileS: `(max-width: ${sizes.mobileS})`,
+  mobileM: `(max-width: ${sizes.mobileM})`,
+  mobileL: `(max-width: ${sizes.mobileL})`,
+  tablet: `(max-width: ${sizes.tablet})`,
+  laptop: `(max-width: ${sizes.laptop})`,
+  laptopL: `(max-width: ${sizes.laptopL})`,
+  desktop: `(min-width: ${sizes.desktop})`,
+};
