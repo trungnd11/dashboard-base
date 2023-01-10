0a134553f114572ac3054eda284fe88e3eda537c
trungnd11 <trungnd1@devcamp.edu.vn>
2023-01-10 23:36:36 +0700

    Update form

--- src/pages/form/FormInstance.tsx
+++ src/pages/form/FormInstance.tsx
@@ -1,5 +1,5 @@
 import { Controller, useForm } from "react-hook-form";
-import { Button, Form, Input } from "antd";
+import { Button, Col, Form, Input, Row } from "antd";
 
 import React from "react";
 
@@ -13,22 +13,26 @@ export default function FormInstance() {
   const onSubmit = (data: { email: string }) => console.log(data);
 
   return (
-    <Form onFinish={handleSubmit(onSubmit)}>
-      <Form.Item
-        label="Email"
-        validateStatus={errors?.email ? "error" : "success"}
-        help={errors?.email ? <span>{errors?.email?.message }</span> : null}
-      >
-        <Controller
-          name="email"
-          defaultValue=""
-          control={control}
-          rules={{ required: "Email is required", maxLength: { value: 5, message: "Email is long" } }}
-          render={({ field }) => (
-            <Input { ...field } />
-          )} />
-      </Form.Item>
-      <Button htmlType="submit">Submit</Button>
-    </Form>
+    <Row>
+      <Col span={6}>
+        <Form onFinish={handleSubmit(onSubmit)}>
+          <Form.Item
+            label="Email"
+            validateStatus={errors?.email ? "error" : "success"}
+            help={errors?.email ? <span>{errors?.email?.message}</span> : null}
+          >
+            <Controller
+              name="email"
+              defaultValue=""
+              control={control}
+              rules={{ required: "Email is required", maxLength: { value: 5, message: "Email is long" } }}
+              render={({ field }) => (
+                <Input {...field} />
+              )} />
+          </Form.Item>
+          <Button htmlType="submit">Submit</Button>
+        </Form>
+      </Col>
+    </Row>
   );
 }
