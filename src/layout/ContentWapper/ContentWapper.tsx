eda37025fceab7794614c4545ade45aeab0ddc0e
trungnd <trung.dn9500@gmail.com>
2023-03-18 09:27:55 +0700

    update

--- src/layout/ContentWapper/ContentWapper.tsx
+++ src/layout/ContentWapper/ContentWapper.tsx
@@ -2,6 +2,15 @@ import React from "react";
 
 export default function ContentWapper() {
   return (
-    <div>ContentWapper</div>
+    <div><p>long content</p>
+      {
+        // indicates very long content
+        Array.from({ length: 100 }, (_, index) => (
+          <React.Fragment key={index}>
+            {index % 20 === 0 && index ? "more" : "..."}
+            <br />
+          </React.Fragment>
+        ))
+      }</div>
   );
 }
